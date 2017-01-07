5c614bf62e0b1168eb6d4a83358a737c2f078efe
Bill Neely <bill.neely.tx@gmail.com>
2017-01-07 15:17:57 -0600

    Part 1 - Build a Basic UI

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Bill Neely on 1/6/17.
+//  Copyright © 2017 Food Demo Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
